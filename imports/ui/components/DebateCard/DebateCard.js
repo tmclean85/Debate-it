a04689f1cbc7407d74a1ee1a74767e5f094389a6
tmclean85 <trevor.a.mclean@gmail.com>
2017-08-03 19:32:29 -0700

    Comment out DebateCard for reuse in the proper container

--- imports/ui/components/DebateCard/DebateCard.js
+++ imports/ui/components/DebateCard/DebateCard.js
@@ -3,20 +3,39 @@ import Gravatar from 'react-gravatar';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import { Link } from 'react-router-dom';
 
-const DebateCard = () => {
-  <div className="debate-card-wrapper">
-    <Card>
-      <CardHeader
-      {/* title={Debate title here} */}
-      {/* subtitle={Debate question here} */}
-      />
-      <div className="debate-card-middle-wrapper">
-        <CardTitle
-          title="Yes"
-        />
-      </div>  
-    </Card>
-  </div>  
-};
+// const DebateCard = () => {
+//   <div className="debate-card-wrapper">
+//     <Card>
+//       <CardHeader
+//       {/* title={Debate title here} */}
+//       {/* subtitle={Debate question here} */}
+//       />
+//       <div className="debate-card-yes-user">
+//         <CardTitle
+//           title="Yes"
+//         />
+//         <Gravatar email="yesusers@email.com" />
+//         <CardText>
+//           <p>Yes user's thesis here</p>
+//         </CardText>  
+//       </div>  
+//       <div className="debate-card-no-user">
+//         <CardTitle
+//           title="Yes"
+//         />
+//         <Gravatar email="nousers@email.com" />
+//         <CardText>
+//           <p>No user's thesis here</p>
+//         </CardText>  
+//       </div>   
+//       <div className="debate-meta">
+//         <h1>Where?</h1>
+//         <p>Here.</p>
+//         <h1>Duration:</h1>
+//         <p>{start time} : {finish time}</p>
+//       </div>       
+//     </Card>
+//   </div>  
+// };
 
 export default DebateCard;
