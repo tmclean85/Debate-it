f5bda791347957b814ec2ec6d2acf761548b4800
tmclean85 <trevor.a.mclean@gmail.com>
2017-08-04 13:46:10 -0700

    Correct extra instance of header

--- imports/ui/containers/Home/Home.js
+++ imports/ui/containers/Home/Home.js
@@ -4,9 +4,6 @@ import Header from '../../components/Header';
 
 const Home = ({ debateData }) => (
     <div className="DebateCardList-wrapper">
-        <div>
-            <Header />
-        </div>
         {/* <Masonry> */}
             {debateData.map(debate =>
                 (<DebateCard debateData={debate} key={debate._id} />)
