93df1a638099035ed7213d42256bde8093ced491
tmclean85 <trevor.a.mclean@gmail.com>
2017-08-07 17:52:38 -0700

    Implement profile page, remove autopublish

--- imports/ui/components/DebateCard/DebateCard.js
+++ imports/ui/components/DebateCard/DebateCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 //import Gravatar from 'react-gravatar';
-import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
--- imports/ui/containers/Profile/Profile.js
+++ imports/ui/containers/Profile/Profile.js
@@ -1,1 +1,35 @@
+import React from 'react';
+import Avatar from 'material-ui/Avatar';
+import {List, ListItem} from 'material-ui/List';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Close from 'material-ui/svg-icons/navigation/close';
+import Check from 'material-ui/svg-icons/navigation/check';
+import Star from 'material-ui/svg-icons/toggle/star';
+
+
+import './styles.css';
+
+const Profile = () => {
+  return (
+  <div className="profile-wrapper">
+    <Card>
+      <div className="profile-main-box">      
+        <Avatar src="" /> 
+        <CardHeader
+          title="User's Name"
+          subtitle="User's Interests"
+        />
+      </div>   
+      <div className="profile-meta-box">
+        <List>
+          <ListItem primaryText="Wins" leftIcon={<Check />} />
+          <ListItem primaryText="Losses" leftIcon={<Close />} />
+          <ListItem primaryText="Good Points" leftIcon={<Star />} />          
+        </List>  
+      </div>  
+    </Card>  
+  </div>  
+  )
+}
+
 export default Profile;
