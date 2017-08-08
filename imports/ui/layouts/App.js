f869a9f8b870919cf657558b9d9a3edc3e7f18f3
Charlie <charlie@Charlies-MacBook-Pro.local>
2017-08-08 09:43:09 -0700

    installed redux and associated packages

--- imports/ui/layouts/App.js
+++ imports/ui/layouts/App.js
@@ -1,25 +1,32 @@
 import React, { PropTypes } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Provider } from 'react-redux';
 import { Meteor } from 'meteor/meteor';
 import Home from '../containers/Home';
+import store from '../../redux/store';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import PlinioTests from '../containers/PlinioTests';
 import CurrentDebate from '../containers/CurrentDebate';
 import Layout from '../components/Layout';
 
 
+injectTapEventPlugin();
+
 const App = props => (
   <MuiThemeProvider>
-    <Router>
-          <Switch>
-            <Layout>
-              <Route exact path="/" component={Home} />
-              <Route path="/plinio" component={PlinioTests} />
-              <Route path="/currentDebate" component={CurrentDebate} /> 
-            </Layout>
-            {/* <Route component={NotFound} /> */}
-          </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+            <Switch>
+              <Layout>
+                <Route exact path="/" component={Home} />
+                <Route path="/plinio" component={PlinioTests} />
+                <Route path="/currentDebate" component={CurrentDebate} /> 
+              </Layout>
+              {/* <Route component={NotFound} /> */}
+            </Switch>
+      </Router>
+    </Provider>
   </MuiThemeProvider>
 );
 
