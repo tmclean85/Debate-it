6844fc2e76854c450c0a1864bd902534c982c917
tmclean85 <trevor.a.mclean@gmail.com>
2017-08-10 16:24:55 -0700

    Revert changes to debateAttendees

--- imports/ui/containers/CurrentDebate/DebateAttendees.js
+++ imports/ui/containers/CurrentDebate/DebateAttendees.js
@@ -3,11 +3,9 @@ import { ListItem } from 'material-ui/List';
 import { ToggleCheckBox, ToggleCheckBoxOutlineBlank } from 'material-ui/svg-icons';
 import Avatar from 'material-ui/Avatar';
 
-const DebateAttendees = ({ userData, voteData, icon }) => (
+const DebateAttendees = ({ userData, icon }) => (
     <ListItem
         primaryText={userData.profile.name}
-        // TODO: Access user-specific because values
-        secondaryText={voteData[0].because}
         leftAvatar={<Avatar src="" />}
         rightIcon={icon}
     />
